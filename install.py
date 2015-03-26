62ea95e46caad9942459cf065422e6bd2d2902b9
Riser101 <sy.yousuf9106@gmail.com>
2015-03-26 18:57:29 +0530

    Update install.py
    Removed shell command

--- install.py
+++ install.py
@@ -1,3 +1,22 @@
+import subprocess as sp
 import os
 
-os.system('pip install PyMySQL')
+def hasPYMYSQL():
+   try:
+    sp.check_call(['pip3','--version'])
+    sp.call(['sudo','pip3','install','PyMySQL'])
+    return True
+   except:
+    sp.call(['sudo','apt-get','install','python3-pip'])
+    sp.call(['sudo','pip3','install','PyMySQL'])
+    return False
+hasPYMYSQL()
+
+def hasCEMENT():
+ try:
+  sp.call('pip3','install','cement')
+  return True
+ except:
+  return False
+hasCEMENT()
+ 
